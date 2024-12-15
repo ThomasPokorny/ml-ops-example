020dc2782c58b2c108f7ea09b7a04d399ffd1607
ThomasPokorny <pokornyth@gmail.com>
2024-12-15 16:38:17 +0100

    remove unused imports

--- validation-enrichment-service/src/main/java/com/tp/platform/kafka/KafkaConsumerService.java
+++ validation-enrichment-service/src/main/java/com/tp/platform/kafka/KafkaConsumerService.java
@@ -3,7 +3,6 @@ package com.tp.platform.kafka;
 import com.tp.domain.ConsumptionDataMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
