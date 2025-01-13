e10b6b243be28527e69faac90bb0d135a5d7a577
ThomasPokorny <pokornyth@gmail.com>
2025-01-13 19:46:32 +0100

    add weekly aggregates

---
+++ data-lake-service/src/main/java/com/tp/domain/consumption/aggregates/weekly/WeeklyConsumptionDataService.java
@@ -0,0 +1,48 @@
+package com.tp.domain.consumption.aggregates.weekly;
+
+import com.tp.domain.consumption.ConsumptionDataDto;
+import com.tp.domain.consumption.aggregates.DataAggregateService;
+import com.tp.domain.consumption.aggregates.daily.DailyConsumptionData;
+import com.tp.domain.consumption.aggregates.daily.DailyConsumptionDataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class WeeklyConsumptionDataService implements DataAggregateService {
+    private final WeeklyConsumptionDataRepository repository;
+
+    public void addToAggregate(ConsumptionDataDto dto) {
+        final var date = Date.from(LocalDateTime.parse(dto.dateTime()).toInstant(ZoneOffset.ofHours(0)));
+        final var endOfWeek = getEndOfWeek(date);
+
+        final var aggregate = repository.findByDate(endOfWeek).orElseGet(() -> {
+            final var createAggregate =  new WeeklyConsumptionData();
+            createAggregate.setDevice(dto.device());
+            createAggregate.setValue(0.0);
+            createAggregate.setDate(endOfWeek);
+            return createAggregate;
+        });
+
+        final var aggregatedValue = aggregate.getValue();
+        aggregate.setValue(aggregatedValue + dto.value());
+
+        this.repository.save(aggregate);
+    }
+
+    private Date getEndOfWeek(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTime();
+    }
+}
